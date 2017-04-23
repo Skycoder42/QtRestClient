5734dd20e7c016c37938a16c0aa14c177e374b9e
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-04-23 18:31:37 +0200

    fixed deps

--- repogen.sh
+++ repogen.sh
@@ -4,4 +4,4 @@
 # $2 Version
 
 myDir=$(dirname "$0")
-"$myDir/../QtModules/repogen.py" "$1" RestClient "qt.58.skycoder42.jsonserializer>=2.0.0" "qrestbuilder" "A library for generic JSON-based REST-APIs, with a mechanism to map JSON to Qt objects." "$2" "$myDir/LICENSE" BSD-3-Clause
+"$myDir/../QtModules/repogen.py" "$1" RestClient "qt.58.skycoder42.jsonserializer" "qrestbuilder" "A library for generic JSON-based REST-APIs, with a mechanism to map JSON to Qt objects." "$2" "$myDir/LICENSE" BSD-3-Clause
