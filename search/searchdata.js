2469d1ffdbfe65785ed5a3abb5b4e52468f74634
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-04-23 18:33:15 +0200

    doc update to 1.2.2

--- search/searchdata.js
+++ search/searchdata.js
@@ -1,12 +1,12 @@
 var indexSectionsWithContent =
 {
   0: "_abcdefghijklmnopqrstuvwxyz~",
-  1: "abcdefgiklmnopqrstuvw",
-  2: "q",
+  1: "abcdefghiklmnopqrstuvw",
+  2: "_q",
   3: "rs",
   4: "abcdefghijklmnopqrstuvwxyz~",
   5: "_abcdefghiklmnopqrstuvw",
-  6: "chio",
+  6: "chijot",
   7: "cdemrs",
   8: "dfjn",
   9: "abcdefghiklmnopqrstuvwxyz",
