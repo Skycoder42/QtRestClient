e6aad8b08f19bf753e1e27ca1ef3c8d05440655f
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-16 00:52:32 +0200

    fix docker?

--- tests/ci/setup-docker.sh
+++ tests/ci/setup-docker.sh
@@ -3,7 +3,9 @@ set -ex
 
 apt-get -qq update
 apt-get -qq install --no-install-recommends curl
-curl -sL https://deb.nodesource.com/setup_6.x | bash -
+curl -Lo /tmp/instnode.sh https://deb.nodesource.com/setup_6.x
+chmod a+x /tmp/instnode.sh
+/tmp/instnode.sh
 apt-get install -y nodejs
 which nodejs
 which npm
