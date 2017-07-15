949ad3a22acb92ce17ef9626872f349edcf647b0
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-16 00:39:12 +0200

    .

--- tests/ci/setup-docker.sh
+++ tests/ci/setup-docker.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+set -ex
 
 apt-get -qq update
 apt-get -qq install --no-install-recommends curl
 curl -sL https://deb.nodesource.com/setup_6.x | bash -
 apt-get install -y nodejs
+which nodejs
+which npm
 npm install -g json-server
-
-$(dirname "$0")/setup-docker-main.sh
--- tests/ci/setup.sh
+++ tests/ci/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 if [[ $PLATFORM == "gcc_64" ]]; then
-	mv ./qtmodules-travis/ci/linux/setup-docker.sh ./qtmodules-travis/ci/linux/setup-docker-main.sh
+	mv ./qtmodules-travis/ci/linux/setup-docker.sh ./qtmodules-travis/ci/linux/setup-docker.sh.tmp
 	mv $(dirname "$0")/setup-docker.sh ./qtmodules-travis/ci/linux/setup-docker.sh
+	cat ./qtmodules-travis/ci/linux/setup-docker.sh.tmp >> ./qtmodules-travis/ci/linux/setup-docker.sh
 else
 	if [[ $TRAVIS_OS_NAME == "linux" ]]; then
 		curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
