d46d4b9824d5a753f7a2b6a0e94a9397bf610d13
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-16 00:34:31 +0200

    .

--- tests/ci/setup-docker.sh
+++ tests/ci/setup-docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_6.x | -E bash -
+apt-get -qq update
+apt-get -qq install --no-install-recommends curl
+curl -sL https://deb.nodesource.com/setup_6.x | bash -
 apt-get install -y nodejs
 npm install -g json-server
 
