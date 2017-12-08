3d6a35111be6ab87c4766f2be95343f4e8ad4112
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-12-08 23:31:48 +0100

    update repogen [skip ci]

--- repogen.sh
+++ repogen.sh
@@ -8,4 +8,4 @@ qtDir=${1?First parameter must be set to the dir to install}
 version=${2?Set the version as second parameter}
 qtvid=$(echo $qtDir | sed -e "s/\\.//g")
 
-"$myDir/../QtModules/deploy/repogen.py"  "$qtDir" RestClient "qt.$qtvid.skycoder42.jsonserializer" "qrestbuilder" "A library for generic JSON-based REST-APIs, with a mechanism to map JSON to Qt objects." "$version" "$myDir/LICENSE" BSD-3-Clause
+"$myDir/../QtModules/deploy/repogen.py"  "$qtDir" RestClient "qt.qt5.$qtvid.skycoder42.jsonserializer" "qrestbuilder" "A library for generic JSON-based REST-APIs, with a mechanism to map JSON to Qt objects." "$version" "$myDir/LICENSE" BSD-3-Clause
